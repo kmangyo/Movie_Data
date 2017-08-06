5a9aaa0aac47e1aa854bbf219c25ccfd908feea4
Hyun Gyo, Kim <khg423@gmail.com>
2017-08-06 22:57:05 +0900

    Update words_compare.r

--- spider_man/words_compare.r
+++ spider_man/words_compare.r
@@ -26,6 +26,7 @@ point_17<-list()
 comments_17<-list()
 date_17<-list()
 
+# by 250
 for (i in 751:1000) {
   point_17[[i]]<-read_html(iconv(url_2017[i], from = "euc-kr", to = "CP949"), encoding = "CP949") %>% html_nodes('.point') %>% html_text()
   comments_17[[i]]<-read_html(iconv(url_2017[i], from = "euc-kr", to = "CP949"), encoding = "CP949") %>% html_nodes('.title') %>% html_text()
@@ -48,7 +49,7 @@ comments_17_df <- melt(comments_17)
 comments_17_df$value<-as.character(comments_17_df$value)
 comments_12_df$value<-as.character(comments_12_df$value)
 
-#
+# extract noun using KonLP
 text.noun_12<- list()
 for (i in 1:nrow(comments_12_df) ) {
   text.noun_12[i]<-melt(extractNoun(comments_12_df[i,1]))
@@ -70,6 +71,7 @@ text.noun_df<-rbind(text.noun_12_df,text.noun_17_df)
 text.noun_df_hist<- text.noun_df %>% count(group, value)
 text.noun_df_hist<- text.noun_df_hist %>% arrange(-n)
 
+# compare nouns
 text.noun_df_count <- text.noun_df %>%
   count(group, value) %>% filter(n>100) %>% 
   spread(group, n, fill = 0) %>%
@@ -86,9 +88,7 @@ tail(text.noun_df_count, 20)
 ggplot(head(text.noun_df_count, 20), aes(x=reorder(value, log_ratio), y=log_ratio)) + geom_bar(stat = "identity") + coord_flip()
 ggplot(tail(text.noun_df_count, 20), aes(x=reorder(value, -log_ratio), y=log_ratio)) + geom_bar(stat = "identity") + coord_flip()
 
-
-# using python
-
+# using python data
 text.noun_p_12_df <-read.csv(file.choose(), encoding = 'UTF-8')
 head(text.noun_p_12_df)
 text.noun_p_17_df <-read.csv(file.choose(), encoding = 'UTF-8')
@@ -112,6 +112,7 @@ head(text.noun_p_df)
 text.noun_p_df_hist<- text.noun_p_df %>% count(group, tag_noun)
 text.noun_p_df_hist<- text.noun_p_df_hist %>% arrange(-n)
 
+# compare nouns
 text.noun_p_df_count <- text.noun_p_df %>% filter(tag_noun_n>1) %>%
   count(group, tag_noun) %>% filter(n>100) %>% 
   spread(group, n, fill = 0) %>%
@@ -127,4 +128,3 @@ tail(text.noun_p_df_count, 20)
 
 ggplot(head(text.noun_p_df_count, 20), aes(x=reorder(tag_noun, log_ratio), y=log_ratio)) + geom_bar(stat = "identity") + coord_flip()
 ggplot(tail(text.noun_p_df_count, 20), aes(x=reorder(tag_noun, -log_ratio), y=log_ratio)) + geom_bar(stat = "identity") + coord_flip()
-
